314cdffc6f50f3669a6b53a18177eaeab50bcff1
John <john@example.com>
2022-07-24 19:58:44 +0000

    Add hello world text.

--- web-app.cpp
+++ web-app.cpp
@@ -6,5 +6,15 @@ using namespace std;
 
 int main() {
     cout << "<!doctype html>\n";
+    cout << "<html lang=\"en\">\n";
+    cout << "  <head>\n";
+    cout << "    <meta charset=\"utf-8\">\n";
+    cout << "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
+    cout << "    <title>Customer portal</title>\n";
+    cout << "  </head>\n";
+    cout << "  <body>\n";
+    cout << "    <h1>Hello, world!</h1>\n";
+    cout << "  </body>\n";
+    cout << "</html>\n";
     return 0;
 }
